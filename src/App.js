bc75dc62391a27e3a2201d92c9226e28e4bf2c01
chendechuan <chen0802@qq.com>
2024-09-30 14:58:21 +0800

    1. 设置 state 只会为下一次渲染变更 state 的值；

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState("Hi!");
-  if (isSent) {
-    return <h1>Your message is on its way!</h1>;
-  }
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        setIsSent(true);
-        sendMessage(message);
-      }}
-    >
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
-  );
-}
+export default function Counter() {
+  const [number, setNumber] = useState(0);
 
-function sendMessage(message) {
-  // ...
+  return (
+    <>
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+          setNumber(number + 2);
+          setNumber(number + 3);
+        }}
+      >
+        +3
+      </button>
+    </>
+  );
 }
