5564a81c6bcc7d925d33bd9728d1af1500b38471
chendechuan <chen0802@qq.com>
2024-09-30 16:59:08 +0800

    1. React 用来计算最终 state 的算法；

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import { useState } from "react";
+import { getFinalState } from "./processQueue.js";
 
-export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+function increment(n) {
+  return n + 1;
+}
+increment.toString = () => "n => n+1";
 
-  async function handleClick() {
-    setPending((pending) => pending + 1);
-    await delay(3000);
-    setPending((pending) => pending - 1);
-    setCompleted((completed) => completed + 1);
-  }
-
+export default function App() {
   return (
     <>
-      <h3>等待：{pending}</h3>
-      <h3>完成：{completed}</h3>
-      <button onClick={handleClick}>购买</button>
+      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
+      <hr />
+      <TestCase
+        baseState={0}
+        queue={[increment, increment, increment]}
+        expected={3}
+      />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment]} expected={6} />
+      <hr />
+      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
     </>
   );
 }
 
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+function TestCase({ baseState, queue, expected }) {
+  const actual = getFinalState(baseState, queue);
+  return (
+    <>
+      <p>
+        初始 state：<b>{baseState}</b>
+      </p>
+      <p>
+        队列：<b>[{queue.join(", ")}]</b>
+      </p>
+      <p>
+        预期结果：<b>{expected}</b>
+      </p>
+      <p
+        style={{
+          color: actual === expected ? "green" : "red",
+        }}
+      >
+        你的结果：<b>{actual}</b> ({actual === expected ? "正确" : "错误"})
+      </p>
+    </>
+  );
 }
