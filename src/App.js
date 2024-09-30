d0e4fc63218cefc6af669d6504aebe41275389f7
chendechuan <chen0802@qq.com>
2024-09-30 15:30:03 +0800

    1. 把 alert 方法放在 setWalk 方法之前或之后;

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [to, setTo] = useState("Alice");
-  const [message, setMessage] = useState("你好");
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`你向 ${to} 说了${message}`);
-    }, 5000);
+  function handleClick() {
+    alert((walk ? "Stop" : "Walk") + " is next");
+    setWalk(!walk);
+    alert((walk ? "Stop" : "Walk") + " is next");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        To:{" "}
-        <select value={to} onChange={(e) => setTo(e.target.value)}>
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
-        </select>
-      </label>
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">发送</button>
-    </form>
+    <>
+      <button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
+      <h1
+        style={{
+          color: walk ? "darkgreen" : "darkred",
+        }}
+      >
+        {walk ? "Walk" : "Stop"}
+      </h1>
+    </>
   );
 }
