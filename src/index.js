5f56fe2f760c879f93426f9bc290cd1ea7a60b83
chendechuan <chen0802@qq.com>
2024-09-30 14:46:45 +0800

    1. React 把更改提交到 DOM 上；

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import Gallery from "./Gallery.js";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Gallery />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
