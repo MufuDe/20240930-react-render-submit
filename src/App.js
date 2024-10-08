449df4490171253dc78d4b203f468de368d43909
chendechuan <chen0802@qq.com>
2024-10-08 11:17:07 +0800

    1. 向数组中添加元素；

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import { useImmer } from "use-immer";
-import Background from "./Background.js";
-import Box from "./Box.js";
+import { useState } from "react";
 
-const initialPosition = {
-  x: 0,
-  y: 0,
-};
+let nextId = 0;
 
-export default function Canvas() {
-  const [shape, updateShape] = useImmer({
-    color: "orange",
-    position: initialPosition,
-  });
-
-  function handleMove(dx, dy) {
-    updateShape((draft) => {
-      draft.position.x += dx;
-      draft.position.y += dy;
-    });
-  }
-
-  function handleColorChange(e) {
-    updateShape((draft) => {
-      draft.color = e.target.value;
-    });
-  }
+export default function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
 
   return (
     <>
-      <select value={shape.color} onChange={handleColorChange}>
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background position={initialPosition} />
-      <Box color={shape.color} position={shape.position} onMove={handleMove}>
-        Drag me!
-      </Box>
+      <h1>振奋人心的雕塑家们：</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          artists.push({
+            id: nextId++,
+            name: name,
+          });
+        }}
+      >
+        添加
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
