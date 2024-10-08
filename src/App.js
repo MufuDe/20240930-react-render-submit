c154c9ce95255d8504176a54df901f851fbc9b25
chendechuan <chen0802@qq.com>
2024-10-08 15:38:47 +0800

    1. 转换数组；

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import { useState } from "react";
 
-let initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louise Nevelson" },
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
 ];
 
-export default function List() {
-  const [artists, setArtists] = useState(initialArtists);
+export default function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
+
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // 不作改变
+        return shape;
+      } else {
+        // 返回一个新的圆形，位置在下方 50px 处
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // 使用新的数组进行重渲染
+    setShapes(nextShapes);
+  }
 
   return (
     <>
-      <h1>振奋人心的雕塑家们：</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>
-            {artist.name}{" "}
-            <button
-              onClick={() => {
-                // 对于 artists 数组中的每一个元素，filter 方法都会调用提供的回调函数，
-                // 并将当前元素作为参数传递给这个回调函数。
-                // 在这个例子中，这个参数被命名为 a。
-                setArtists(artists.filter((a) => a.id !== artist.id));
-              }}
-            >
-              删除
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>所有圆形向下移动！</button>
+      {shapes.map((shape) => (
+        <div
+          key={shape.id}
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
     </>
   );
 }
