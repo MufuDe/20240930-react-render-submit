fc7e05ff23333ca21a89e1018ab5a6b9f084a830
chendechuan <chen0802@qq.com>
2024-10-08 15:35:11 +0800

    1. 从数组中删除元素；

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import { useState } from "react";
 
-let nextId = 0;
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
     <>
       <h1>振奋人心的雕塑家们：</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          setArtists(
-            // 替换 state
-            [
-              // 是通过传入一个新数组实现的
-              ...artists, // 新数组包含原数组的所有元素
-              { id: nextId++, name: name }, // 并在末尾添加了一个新的元素
-            ]
-          );
-        }}
-      >
-        添加
-      </button>
       <ul>
         {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                // 对于 artists 数组中的每一个元素，filter 方法都会调用提供的回调函数，
+                // 并将当前元素作为参数传递给这个回调函数。
+                // 在这个例子中，这个参数被命名为 a。
+                setArtists(artists.filter((a) => a.id !== artist.id));
+              }}
+            >
+              删除
+            </button>
+          </li>
         ))}
       </ul>
     </>
