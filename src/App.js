f91ae58186be5e554630202531ab9b9341a1559a
chendechuan <chen0802@qq.com>
2024-10-08 11:19:38 +0800

    1. 使用 ... 数组展开 语法；

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ export default function List() {
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button
         onClick={() => {
-          artists.push({
-            id: nextId++,
-            name: name,
-          });
+          setArtists(
+            // 替换 state
+            [
+              // 是通过传入一个新数组实现的
+              ...artists, // 新数组包含原数组的所有元素
+              { id: nextId++, name: name }, // 并在末尾添加了一个新的元素
+            ]
+          );
         }}
       >
         添加
