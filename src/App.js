a4d95c0afde1b7a1879a8a2f27ca9ec49a3eeb90
chendechuan <chen0802@qq.com>
2024-10-08 15:41:37 +0800

    1. 替换数组中的元素；

--- src/App.js
+++ src/App.js
@@ -1,48 +1,37 @@
 import { useState } from "react";
 
-let initialShapes = [
-  { id: 0, type: "circle", x: 50, y: 100 },
-  { id: 1, type: "square", x: 150, y: 100 },
-  { id: 2, type: "circle", x: 250, y: 100 },
-];
+let initialCounters = [0, 0, 0];
 
-export default function ShapeEditor() {
-  const [shapes, setShapes] = useState(initialShapes);
+export default function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
 
-  function handleClick() {
-    const nextShapes = shapes.map((shape) => {
-      if (shape.type === "square") {
-        // 不作改变
-        return shape;
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // 递增被点击的计数器数值
+        return c + 1;
       } else {
-        // 返回一个新的圆形，位置在下方 50px 处
-        return {
-          ...shape,
-          y: shape.y + 50,
-        };
+        // 其余部分不发生变化
+        return c;
       }
     });
-    // 使用新的数组进行重渲染
-    setShapes(nextShapes);
+    setCounters(nextCounters);
   }
 
   return (
-    <>
-      <button onClick={handleClick}>所有圆形向下移动！</button>
-      {shapes.map((shape) => (
-        <div
-          key={shape.id}
-          style={{
-            background: "purple",
-            position: "absolute",
-            left: shape.x,
-            top: shape.y,
-            borderRadius: shape.type === "circle" ? "50%" : "",
-            width: 20,
-            height: 20,
-          }}
-        />
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            +1
+          </button>
+        </li>
       ))}
-    </>
+    </ul>
   );
 }
