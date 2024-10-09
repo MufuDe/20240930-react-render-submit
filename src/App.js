d2f1291a9dca67b326009b39a8e89edc9701c2b0
chendechuan <chen0802@qq.com>
2024-10-09 14:14:43 +0800

    1. 按下“+”时递增对应数字;

--- src/App.js
+++ src/App.js
@@ -1,57 +1,50 @@
 import { useState } from "react";
-import { useImmer } from "use-immer";
 
-let nextId = 3;
-const initialList = [
-  { id: 0, title: "Big Bellies", seen: false },
-  { id: 1, title: "Lunar Landscape", seen: false },
-  { id: 2, title: "Terracotta Army", seen: true },
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
 ];
 
-export default function BucketList() {
-  const [myList, updateMyList] = useImmer(initialList);
-  const [yourList, updateYourList] = useImmer(initialList);
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
 
-  function handleToggleMyList(id, nextSeen) {
-    updateMyList((draft) => {
-      const artwork = draft.find((a) => a.id === id);
-      artwork.seen = nextSeen;
-    });
+  function handleIncreaseClick(productId) {
+    setProducts(
+      products.map((product) => {
+        if (product.id === productId) {
+          return { ...product, count: product.count + 1 };
+        } else {
+          return product;
+        }
+      })
+    );
   }
 
-  function handleToggleYourList(artworkId, nextSeen) {
-    updateYourList((draft) => {
-      const artwork = draft.find((a) => a.id === artworkId);
-      artwork.seen = nextSeen;
-    });
-  }
-
-  return (
-    <>
-      <h1>艺术愿望清单</h1>
-      <h2>我想看的艺术清单：</h2>
-      <ItemList artworks={myList} onToggle={handleToggleMyList} />
-      <h2>你想看的艺术清单：</h2>
-      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
-    </>
-  );
-}
-
-function ItemList({ artworks, onToggle }) {
   return (
     <ul>
-      {artworks.map((artwork) => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={(e) => {
-                onToggle(artwork.id, e.target.checked);
-              }}
-            />
-            {artwork.title}
-          </label>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
         </li>
       ))}
     </ul>
