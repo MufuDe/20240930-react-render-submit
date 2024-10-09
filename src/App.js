12c798ca686a8e94feece70b56dfca0e8199f428
chendechuan <chen0802@qq.com>
2024-10-09 14:29:45 +0800

    1. 使用不会直接修改原始值的方法修复 mutation 的问题;

--- src/App.js
+++ src/App.js
@@ -1,80 +1,52 @@
 import { useState } from "react";
+import AddTodo from "./AddTodo.js";
+import TaskList from "./TaskList.js";
 
-const initialProducts = [
-  {
-    id: 0,
-    name: "Baklava",
-    count: 1,
-  },
-  {
-    id: 1,
-    name: "Cheese",
-    count: 5,
-  },
-  {
-    id: 2,
-    name: "Spaghetti",
-    count: 2,
-  },
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: "Buy milk", done: true },
+  { id: 1, title: "Eat tacos", done: false },
+  { id: 2, title: "Brew tea", done: false },
 ];
 
-export default function ShoppingCart() {
-  const [products, setProducts] = useState(initialProducts);
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
 
-  function handleIncreaseClick(productId) {
-    setProducts(
-      products.map((product) => {
-        if (product.id === productId) {
-          return {
-            ...product,
-            count: product.count + 1,
-          };
+  function handleAddTodo(title) {
+    setTodos([
+      ...todos,
+      {
+        id: nextId++,
+        title: title,
+        done: false,
+      },
+    ]);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    setTodos(
+      todos.map((t) => {
+        if (t.id === nextTodo.id) {
+          return nextTodo;
         } else {
-          return product;
+          return t;
         }
       })
     );
   }
 
-  function handleSubtractClick(productId) {
-    let mapProducts = products.map((product) => {
-      if (product.id === productId) {
-        return {
-          ...product,
-          count: product.count - 1,
-        };
-      } else {
-        return product;
-      }
-    });
-    setProducts(
-      mapProducts.filter((product) => {
-        return product.count > 0;
-      })
-    );
+  function handleDeleteTodo(todoId) {
+    setTodos(todos.filter((t) => t.id !== todoId));
   }
 
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          {product.name} (<b>{product.count}</b>)
-          <button
-            onClick={() => {
-              handleIncreaseClick(product.id);
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              handleSubtractClick(product.id);
-            }}
-          >
-            –
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
+    </>
   );
 }
